c125056214a8722c8abd4d26cd50b46de6fb2101
ekodemecheng <demecheng@lenteranusa.com>
2024-09-26 13:42:26 +0700

    Add files via upload

---
+++ Type String, Numerik, dan Boolean.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# Type String 
+print('Halo Takumi') #membuat string dengan tanda petik tunggal
+print("Halo Takumi") #membuat string dengan tanda petik tunggal
+
+
+# In[6]:
+
+
+# Variabel Type String
+s1 = 'Halo' #variabel string 1
+s2 = 'Takumi' #variabel string 2
+
+# menampilkan type string
+print(s1)
+print(s2)
+print(s1 +' '+ s2)
+
+
+# In[8]:
+
+
+# Membandingkan string
+s1 = 'Halo' #variabel string 1
+s2 = 'Takumi' #variabel string 2
+
+print(s1 > s2)
+print(s1 >= s2)
+print(s1 < s2)
+print(s1 <= s2)
+print(s1 != s2)
+
+
+# In[12]:
+
+
+# Mengambil Substring; menggunakan tanda bracket []
+
+s = 'Takumi'
+
+print(s[0])  #menampilkan sub string karakter pertama
+print(s[-4])  #menampilkan sub string karakter ke 4 dari kanan
+print(s[:2])  #menampilkan dua karakter di awal
+print(s[:-2]) #menghilangkan dua karakter terakir
+print(s[3:])  #menampilkan tiga karakter terakhir
+print(s[-2:]) #menampilkan dua karakter terakhir
+print(s[0:3]) #
+
+
+# In[ ]:
+
+
+
+
